3594e7444a2f0806fefeaf1322e9422bc12314d7
CymoP <p1620013x@my365.dmu.ac.uk>
2022-09-03 22:59:13 +0100

    style(*): reformat code as per google java styling standard

--- src/main/java/uk/gov/dwp/uc/pairtest/domain/TicketTypeRequest.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/domain/TicketTypeRequest.java
@@ -6,24 +6,23 @@ package uk.gov.dwp.uc.pairtest.domain;
 
 public class TicketTypeRequest {
 
-    private int noOfTickets;
-    private Type type;
+  private int noOfTickets;
+  private Type type;
 
-    public TicketTypeRequest(Type type, int noOfTickets) {
-        this.type = type;
-        this.noOfTickets = noOfTickets;
-    }
+  public TicketTypeRequest(Type type, int noOfTickets) {
+    this.type = type;
+    this.noOfTickets = noOfTickets;
+  }
 
-    public int getNoOfTickets() {
-        return noOfTickets;
-    }
+  public int getNoOfTickets() {
+    return noOfTickets;
+  }
 
-    public Type getTicketType() {
-        return type;
-    }
+  public Type getTicketType() {
+    return type;
+  }
 
-    public enum Type {
-        ADULT, CHILD , INFANT
-    }
-
+  public enum Type {
+    ADULT, CHILD, INFANT
+  }
 }
